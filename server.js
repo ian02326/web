41bdd359940eea1ad1241ff25bab0aa806c08112
kojinghong <xxx100699@gmial.com>
2022-12-30 11:02:45 +0800

    20221230

---
+++ server.js
@@ -0,0 +1,54 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+server = express();
+
+server.use(express.static("moew"));//要跑在伺服器上的首頁母資料夾
+
+server.use(bodyParser.urlencoded());
+server.use(bodyParser.json());
+
+var DB = require("nedb-promises");       //定義資料庫
+var ContactDB = DB.create("contact.db"); //定義資料庫 ContactDB為自命名
+
+
+server.get("/service", function(req, res){
+
+    Services = [
+        { icon: "fa-shopping-cart", title: "E-Commerce", desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur porro laborum fuga repellat necessitatibus corporis nulla, in ex velit recusandae obcaecati maiores, doloremque quisquam similique, tempora aspernatur eligendi delectus! Rem." },
+        { icon: "fa-laptop", title: "Responsive Design", desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime qua architecto quo inventore harum ex magni, dicta impedit." },
+        { icon: "fa-lock", title: "Web Security", desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit." }
+    ]
+    res.send(Services);
+})
+
+server.get("/portfolio", function(req, res){
+    
+    portfolios= [
+        { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
+        { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
+        { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" },
+        { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
+        { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
+        { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" }
+    ]
+   res.send(portfolios);
+})
+
+
+
+server.get("/contact", function(req, res){
+    res.send("");
+    res.redirect("/index Vue.html"); //找檔案時只需要找index.html
+});
+ 
+server.post("/contact", function(req, res){
+    console.log(req.body);
+    ContactDB.insert(req.body);     //加入資料庫
+    res.redirect("/index Vue.html");  //找檔案時只需要找index.html
+})
+
+
+server.listen(8080, function(){
+    console.log("Server is running at port 8080!")
+})
