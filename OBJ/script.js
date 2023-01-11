5d8d7d260e83b335f09687720eac45b4bce6a953
kojinghong <90750570+kojinghong@users.noreply.github.com>
2023-01-12 03:42:53 +0800

    01120342

--- OBJ/script.js
+++ OBJ/script.js
@@ -212,3 +212,16 @@ function removeFollow(delid) {
   follow_list.splice(delid, 1);
   showFollow();
 }
+
+
+// 上架預覽
+document.getElementById('readUrl').addEventListener('change', function () {
+  if (this.files[0]) {
+    var picture = new FileReader();
+    picture.readAsDataURL(this.files[0]);
+    picture.addEventListener('load', function (event) {
+      document.getElementById('uploadedImage').setAttribute('src', event.target.result);
+      document.getElementById('uploadedImage').style.display = 'block';
+    });
+  }
+})
